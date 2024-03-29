4674635a76a4ebd658181dda69849db17d3dbdfc
pedi <pedisdi@gmail.com>
2024-03-29 17:10:23 +0330

    feature: Add validator functions and classes

---
+++ contactmanager/validators/constants.py
@@ -0,0 +1,22 @@
+class Patterns:
+    EMAIL_PATTERN = r"[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    DATE_PATTERN = r"^\d{1,2}-\d{1,2}-\d{1,4}$"
+    NAME_PATTERN = r"^[A-Za-z][a-z1-9]*$"
+    CELLPHONE_NUMBER_PATTERN = r"^(\+98|09)\d-?\d{3}-?\d{5}$"
+    UUID_PATTERN = r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$"
+
+
+class Messages:
+    INVALID_EMAIL_MSG = "Invalid email!"
+    INVALID_DATE_MSG = "Invalid date!"
+    INVALID_NAME_MSG = "Invalid name!"
+    INVALID_CELLPHONE_NUMBER_MSG = "Invalid cellphone number!"
+    INVALID_PRICE_MSG = "Invalid price!"
+    INVALID_UUID = "Invalid UUID '{}'"
+    # User
+    INVALID_PASSWORD_MSG = "Invalid password. Password should be alphanumeric and at least {} characters long."
+    PASSWORD_NOT_MATCHED_MSG = "Password and Confirm Password do not match."
+
+
+class Constants:
+    PASSWORD_MIN_LENGTH = 8
---
+++ contactmanager/validators/validators.py
@@ -0,0 +1,129 @@
+import re
+import datetime
+import hashlib
+
+from . import constants as cs
+
+
+def validate_email(email):
+    if isinstance(email, str) and re.match(cs.Patterns.EMAIL_PATTERN, email):
+        return email
+    raise ValueError(cs.Messages.INVALID_EMAIL_MSG)
+
+
+def validate_date(date_string: str):
+    """
+    date_string = "dd-mm-yyyy"
+    """
+    if isinstance(date_string, str) and re.match(cs.Patterns.DATE_PATTERN, date_string):
+        day, month, year = date_string.split('-')
+        try:
+            datetime.datetime(day=int(day), month=int(month), year=int(year))
+        except ValueError:
+            pass
+        else:
+            return date_string
+
+    raise ValueError(cs.Messages.INVALID_DATE_MSG)
+
+
+def validate_price(price: float | int):
+    """
+
+    :param price:
+    :raise:
+    """
+    if isinstance(price, (float, int)) and price > 0:
+        return price
+    raise ValueError(cs.Messages.INVALID_PRICE_MSG)
+
+
+def validate_name(name: str):
+    if isinstance(name, str) and re.match(cs.Patterns.NAME_PATTERN, name):
+        return name
+    raise ValueError(cs.Messages.INVALID_NAME_MSG)
+
+
+def validate_label(label: str):  # TODO: Implementation
+    return label
+
+
+def validate_index(lst: list, index: int):
+    if isinstance(lst, list) and isinstance(index, int) and len(lst) > index > 0:
+        return True
+    return False
+
+
+def validate_uuid(uuid: str):
+    if isinstance(uuid, str) and re.match(cs.Patterns.UUID_PATTERN, uuid):
+        return uuid
+    raise ValueError(cs.Messages.INVALID_UUID.format(uuid))
+
+
+class User:
+    @staticmethod
+    def validate_password(password: str, confirm_password: str):  # TODO: Implementation
+        if isinstance(password, str) and password.isalnum() and len(password) >= cs.Constants.PASSWORD_MIN_LENGTH:
+            if password == confirm_password:
+                sha256 = hashlib.sha256()
+                sha256.update(password.encode())
+                return sha256.hexdigest()
+            raise ValueError(cs.Messages.PASSWORD_NOT_MATCHED_MSG)
+        raise ValueError(cs.Messages.INVALID_PASSWORD_MSG.format(cs.Constants.PASSWORD_MIN_LENGTH))
+
+    @staticmethod
+    def validate_username(username: str):  # TODO: Implementation
+        return username
+
+
+class Address:
+    @staticmethod
+    def validate_city(city: str):
+        return city
+
+    @staticmethod
+    def validate_address(address: str):  # TODO: Implementation
+        return address
+
+
+class Phones:
+    @staticmethod
+    def validate_cellphone_number(phone_number: str):
+        if isinstance(phone_number, str) and re.match(cs.Patterns.CELLPHONE_NUMBER_PATTERN, phone_number):
+            return phone_number
+        raise ValueError(cs.Messages.INVALID_CELLPHONE_NUMBER_MSG)
+
+    @staticmethod
+    def validate_home_number(phone_number: str):  # TODO: Implementation
+        return phone_number
+
+
+if __name__ == '__main__':
+    try:
+        print("---------------------------------")
+        # print(validate_email("yew(ksad@gmail.com"))
+        # print(validate_email("yew2ksad@g*mail.com"))
+        print(validate_email("yew2ksad@gmail.com"))
+        print("---------------------------------")
+        # print(validate_date("32-2-2002"))
+        # print(validate_date("3-23-2002"))
+        # print(validate_date("3-2-20002"))
+        # print(validate_date("32-2--2002"))
+        # print(validate_date("32-a-2002"))
+        # print(validate_date("32*2*2002"))
+        print(validate_date("3-2-2002"))
+        print("---------------------------------")
+        # print(validate_price(-72))
+        print(validate_price(72))
+        print("---------------------------------")
+        # print(NumberValidator.validate_cellphone_number("+12345678911"))
+        # print(NumberValidator.validate_cellphone_number("19193636442"))
+        # print(NumberValidator.validate_cellphone_number("091903636442"))
+        print(Phones.validate_cellphone_number("+98913636442"))
+        print(Phones.validate_cellphone_number("09193636442"))
+        print("---------------------------------")
+        # print(validate_name("hasan2"))
+        print(validate_name("Hasan"))
+        print(validate_name("hasan"))
+    except ValueError as error:
+        print(error)
