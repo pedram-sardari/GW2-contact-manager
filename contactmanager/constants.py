3ec26f0b0c68ce1297ad909c8a5467cb8acb32f2
pedi <pedisdi@gmail.com>
2024-03-29 13:59:44 +0330

    feature: Add pickleHandler class

---
+++ contactmanager/constants.py
@@ -0,0 +1,28 @@
+class Paths:
+    # User
+    CONTACTS_DIRECTORY_PATH = './db/contacts'
+    USERS_FILE_PATH = './db/users.pickle'
+    LAST_LOGIN_PICKLE_FILE_PATH = './db/last_login_data.pickle'
+
+
+class Constants:
+    LOGIN_DURATION_SEC = 1000000 * 60
+
+
+class Messages:
+    # User
+    USER_ALREADY_EXISTS_MSG = "Username '{}' already exists."
+    INVALID_USERNAME_OR_PASSWORD_MSG = "Invalid username or password. If you haven't registered, please register first."
+    INVALID_USER_ID_MSG = "User with id {} not found"
+    LOGIN_TIMEOUT_MSG = "Login Timeout Expired and there's no logged-in user, please login again later."
+
+    # PickleHandler
+    INVALID_MODE_MSG = 'Invalid mode, must be one of these {}'
+    CANNOT_READ_MSG = 'Cannot read write-modded pickle file'
+    CANNOT_WRITE_MSG = 'Cannot write to read-modded pickle file'
+    FILE_NOT_FOUND_MSG = "File '{}' does not exist."
+
+    # Contact
+    CONTACT_EXIST_ALREADY = 'The same contact already exists'
+    INVALID_PHONE_NUMBER_MSG = "Phone number '{}' is not valid"
+
